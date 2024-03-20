338dbe2193fd0595d46464d9088664a5697e01a3
Abdulxd <tartilaci@gmail.com>
2024-03-20 14:39:02 +0100

    finish project

--- src/App.js
+++ src/App.js
@@ -5,19 +5,39 @@ import data from './helper/data';
 function App() {
   const [employees, setEmployees] = useState(data);
   // console.log(data);
-  
+  const [from, setFrom] = useState(1);
+  const [to, setTo] = useState(5)
+
+  const handlePrev =()=>{
+    if(from>1 && to>5) {
+      setFrom(from-5)
+      setTo(to-5)
+    }
+  }
+
+  const handleNext =()=>{
+    if(from<16 && to<20) {
+      setFrom(from+5)
+      setTo(to+5)
+    }
+  }
+
+  const filterEmployees =(arr)=>{
+    const newArray = arr.slice(from-1, to)
+    return newArray
+  }
  
   return (
     <main>
       <section className="container">
         <h3>Employee List</h3>
         <h5>
-          (Employees 1 to 5)
+          (Employees {from} to {to})
         </h5>
-        <List employees={employees} />
+        <List employees={filterEmployees(employees)}/>
         <div className='btns'>
-          <button >Prev</button>
-          <button >Next</button>
+          <button onClick={handlePrev} >Prev</button>
+          <button onClick={handleNext}>Next</button>
         </div>
       </section>
     </main>
