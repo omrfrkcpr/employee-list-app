72225f429a3f5bac445c3f9a739cf8ba67d63c7d
omrfrkcpr <omerrfarukcapur@gmail.com>
2024-03-20 17:05:15 +0100

    chore: add Person.jsx style and functionality

--- src/components/List.js
+++ src/components/List.js
@@ -2,31 +2,34 @@ import React from "react";
 import { RiFileInfoLine } from "react-icons/ri";
 import { useNavigate } from "react-router-dom";
 
-
-const List = ({employees}) => {
+const List = ({ employees }) => {
   console.log(employees);
 
-  const navigate = useNavigate()
-  
- 
+  const navigate = useNavigate();
+
   return (
     <>
-    {employees.map(({id, name, age, image, email})=>(
-    
-      <article key={id} className="person">
-        <img src={image} alt={name} />
-        <div>
-          <h4>{name}</h4>
+      {employees.map(({ id, name, age, image, email, cv, salary }) => (
+        <article key={id} className="person">
+          <img src={image} alt={name} />
+          <div>
+            <h4>{name}</h4>
 
-          <button onClick={()=> navigate(`/Person/${name}`, {state:{id,name,age, image, email}}) }><RiFileInfoLine 
-          style={{fontSize:'20px', width:'45px'}}/></button>
-          
-          <p>{email}</p>
-          <p>{age} years</p>
-          
-        </div>
-      </article>
-    ))}
+            <button
+              onClick={() =>
+                navigate(`/Person/${name}`, {
+                  state: { id, name, age, image, email, cv, salary },
+                })
+              }
+            >
+              <RiFileInfoLine style={{ fontSize: "20px", width: "45px" }} />
+            </button>
+
+            <p>{email}</p>
+            <p>{age} years</p>
+          </div>
+        </article>
+      ))}
     </>
   );
 };
--- src/components/Person.jsx
+++ src/components/Person.jsx
@@ -1,25 +1,100 @@
-import React from 'react'
-import { useNavigate, useParams } from 'react-router-dom'
-import data from '../helper/data'
+import React, { useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import data from "../helper/data";
 
 const Person = () => {
-  
-    const {username} = useParams()
-    const navigate = useNavigate()
+  const { username } = useParams();
+  const navigate = useNavigate();
+  const [money, setMoney] = useState("en");
 
-    return (
-    <div>
-    {data.map(({name, email, image, id})=>name===username &&(
-    <div key={id}>
-        <img src={image} alt="" />
-        <h3>{name}</h3>
-        <h4>{email}</h4>
-        <button onClick={()=>navigate(-1)} >GO HOME</button>
+  const exchangeRateFromDollarToEuro = 0.95;
+
+  const handleMoneyChange = (e) => {
+    setMoney(e.target.value);
+  };
+
+  return (
+    <div
+      style={{
+        textAlign: "center",
+        margin: "5rem",
+        border: "1px solid lightgrey",
+        padding: "0.5rem 2rem",
+        backgroundColor: "whitesmoke",
+      }}
+    >
+      {data.map(
+        ({ name, email, image, id, cv, salary }) =>
+          name === username && (
+            <div key={id}>
+              <img src={image} alt="" />
+              <h3 style={{ textDecoration: "underline" }}>{name}</h3>
+              <div style={{ display: "flex", justifyContent: "center" }}>
+                <h3>
+                  Salary :{" "}
+                  <span style={{ color: "red" }}>
+                    {money === "en" && "$"}
+                    {money === "en"
+                      ? salary
+                      : salary * exchangeRateFromDollarToEuro}
+                    {money === "de" && "€"}
+                  </span>
+                </h3>
+                <select
+                  value={money}
+                  onChange={handleMoneyChange}
+                  style={{
+                    height: "25px",
+                    marginLeft: ".5rem",
+                    outline: "none",
+                    border: "none",
+                    borderRadius: "10px",
+                  }}
+                >
+                  <option selected value="en">
+                    💵Dollar
+                  </option>
+                  <option value="de">💶Euro</option>
+                </select>
+              </div>
+              <h4
+                style={{
+                  width: "60%",
+                  margin: "auto",
+                  backgroundColor: "white",
+                }}
+              >
+                <a href={`mailto:${email}`}>{email}</a>
+              </h4>
+              <h5 style={{ fontFamily: "italic" }}>
+                <span
+                  style={{
+                    color: "orange",
+                    fontWeight: "bolder",
+                    textDecoration: "underline",
+                  }}
+                >
+                  CV:
+                </span>{" "}
+                {cv}
+              </h5>
+              <button
+                onClick={() => navigate(-1)}
+                style={{
+                  padding: "0.2rem 0.7rem",
+                  border: "none",
+                  backgroundColor: "green",
+                  color: "white",
+                  borderRadius: "10px",
+                }}
+              >
+                GO HOME
+              </button>
+            </div>
+          )
+      )}
     </div>
-    ))}
+  );
+};
 
-    </div>
-  )
-}
-
-export default Person
+export default Person;
